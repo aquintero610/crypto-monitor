fcbcdc56fee149371b0805e7d2a40b2437bb5932
Andrew Quintero <andrewquintero@Andrews-MacBook-Pro.local>
2017-12-10 18:15:38 -0500

    added a refresh timer

--- crypto-monitor/ViewController.swift
+++ crypto-monitor/ViewController.swift
@@ -14,7 +14,7 @@ struct Coin: Decodable {
 }
 
 class ViewController: UIViewController {
-    
+    var timer = Timer()
     //price labels for our 3 coins
     @IBOutlet weak var btcPrice: UILabel!
     @IBOutlet weak var ethPrice: UILabel!
@@ -23,34 +23,37 @@ class ViewController: UIViewController {
     //store price coin information in this array
     var coins = [Coin]()
     
-    //price above and below variables
+    //price textboxes for above and below
     @IBOutlet weak var priceBelow: UITextField!
     @IBOutlet weak var priceAbove: UITextField!
     
+    //this is where we will store the price alerts
+    var mPriceBelowAlert: Double = -1.0
+    var mPriceAboveAlert: Double = -1.0
+    
+    //counter
+    var mCounter: Int = 10;
     
     @IBAction func addPriceAlert(_ sender: Any) {
-        
+        mPriceBelowAlert = Double(priceBelow.text!)!
+        mPriceAboveAlert = Double(priceAbove.text!)!
     }
-    
-    
-    
-    
-    
-    
     
     //refresh price fetches new price data and updates the label
     @IBAction func refreshPrice(_ sender: Any) {
         fetchPriceData()
+        setDataPrice()
+    }
+    
+    func setDataPrice(){
         self.btcPrice.text = self.coins[0].price_usd
         self.ethPrice.text = self.coins[1].price_usd
         self.iotaPrice.text = self.coins[3].price_usd
     }
-
     
     //fetch price data parses json file from coinmarket and updates "coins" with
     //the latest prices
     func fetchPriceData(){
-        
       // Do any additional setup after loading the view, typically from a nib.
       let jsonURL = "https://api.coinmarketcap.com/v1/ticker/"
       let url = URL(string: jsonURL);
@@ -67,9 +70,22 @@ class ViewController: UIViewController {
       }.resume()
     }
     
+    @objc func countdownRefresh(){
+        mCounter = mCounter - 1
+        print(mCounter);
+        if(0 == mCounter){
+            mCounter = 10;
+            self.fetchPriceData()
+            self.setDataPrice()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchPriceData();
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdownRefresh), userInfo: nil, repeats: true)
+     
+        
         
     }
 
